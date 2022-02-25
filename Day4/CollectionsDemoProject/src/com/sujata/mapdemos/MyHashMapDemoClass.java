f6894204fb4d86a6f1165ecf36c0691a4d7fb13a
Sujatabatra <sujatabatra@hotmail.com>
2022-02-25 22:22:14 +0530

    Add files via upload

---
+++ Day4/CollectionsDemoProject/src/com/sujata/mapdemos/MyHashMapDemoClass.java
@@ -0,0 +1,39 @@
+package com.sujata.mapdemos;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MyHashMapDemoClass {
+
+	public static void main(String[] args) {
+
+/*
+ * unordered collection of key value pair which is autogrowable and autoshrinkable,
+ * where keys are unique and values can be duplicate
+ * and unordering as per key
+ */
+		Map<String, Integer> months=new HashMap<String, Integer>();
+		
+		months.put("Jan", 31);
+//		months.put("Feb", 56);
+		months.put("Feb", 28);
+		months.put("Mar", 31);
+		months.put("Apr", 30);
+		months.put("May", 31);
+		months.put("Jun", 30);
+		
+		System.out.println(months);
+		
+		months.put("Feb", 29);
+		System.out.println(months);
+		
+		Set<String> monthsName= months.keySet();
+		for(String monthName:monthsName) {
+			System.out.println(monthName+" has "+months.get(monthName)+" days");
+		}
+		
+		
+	}
+
+}
