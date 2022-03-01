8cfbb36846acde0ac37466fdc45ef50f3d20d69f
Sujatabatra <sujatabatra@hotmail.com>
2022-03-01 22:11:57 +0530

    Add files via upload

---
+++ Day6/MyJDBCEmployeeSpringXMLConfigDemoProject/src/com/sujata/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.sujata.service;
+
+import java.util.List;
+
+import com.sujata.entity.Employee;
+import com.sujata.entity.EmployeePayslip;
+
+public interface EmployeeService {
+
+	List<Employee> getAllEmployee();
+	boolean saveEmployee(Employee employee);
+	Employee getEmployeeById(int id);
+	EmployeePayslip generateSalary(int empId);
+}
