745fd25b65c6a67bb867a971dc62f6398034ae96
Sujatabatra <sujatabatra@hotmail.com>
2022-03-03 16:24:29 +0530

    Add files via upload

---
+++ Day8/MySpringBootEmployeeJDBCTemplateProject/src/main/java/com/sujata/client/MySpringBootEmployeeJdbcTemplateProjectApplication.java
@@ -0,0 +1,34 @@
+package com.sujata.client;
+
+import java.util.Scanner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.sujata.presentation.EmployeePresentation;
+
+@SpringBootApplication(scanBasePackages = "com.sujata")
+public class MySpringBootEmployeeJdbcTemplateProjectApplication implements CommandLineRunner{
+
+	@Autowired
+	private EmployeePresentation employeePresentation;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootEmployeeJdbcTemplateProjectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Scanner scanner=new Scanner(System.in);
+		while(true) {
+			employeePresentation.showMenu();
+			System.out.println("Enter Choice : ");
+			int choice=scanner.nextInt();
+			employeePresentation.performMenu(choice);
+		}
+		
+	}
+
+}
