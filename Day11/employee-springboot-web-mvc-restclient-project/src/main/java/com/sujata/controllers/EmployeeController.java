b90ac76a1020f967d9dc1a39d602a53174d60c64
Sujatabatra <sujatabatra@hotmail.com>
2022-03-08 20:21:04 +0530

    Add files via upload

--- Day11/employee-springboot-web-mvc-restclient-project/src/main/java/com/sujata/controllers/EmployeeController.java
+++ Day11/employee-springboot-web-mvc-restclient-project/src/main/java/com/sujata/controllers/EmployeeController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -38,21 +39,48 @@ public class EmployeeController {
 		return modelAndView;
 	}
 	
+//	@RequestMapping("/insertEmployeePage")
+//	public String insertEmployeePageController() {
+//		return "inputEmployeeDetails";
+//	}
+	
+	
+//	@RequestMapping("/saveEmployee")
+//	public ModelAndView insertEmployeeController(HttpServletRequest request) {
+//		ModelAndView modelAndView=new ModelAndView();
+//		
+//		Employee employee=new Employee();
+//		employee.setEmpId(Integer.parseInt(request.getParameter("empId")));
+//		employee.setEmpName(request.getParameter("empName"));
+//		employee.setEmpDesignation(request.getParameter("empDesignation"));
+//		employee.setEmpDepartment(request.getParameter("empDepartment"));
+//		employee.setEmpSalary(Integer.parseInt(request.getParameter("empSalary")));
+//		
+//		String message=null;
+//		if(employeeService.saveEmployee(employee))
+//			message="Employee Added Succesfully";
+//		else
+//			message="Employee Addition Failed";
+//		
+//		modelAndView.addObject("message", message);
+//		modelAndView.setViewName("output");
+//		
+//		return modelAndView;
+//	}
+	
 	@RequestMapping("/insertEmployeePage")
-	public String insertEmployeePageController() {
-		return "inputEmployeeDetails";
+	public ModelAndView insertEmployeePageController() {
+		ModelAndView modelAndView=new ModelAndView();
+		
+		modelAndView.addObject("emp", new Employee());
+		modelAndView.setViewName("inputEmployeeDetails");
+		
+		return modelAndView;
 	}
 	
 	@RequestMapping("/saveEmployee")
-	public ModelAndView insertEmployeeController(HttpServletRequest request) {
+	public ModelAndView insertEmployeeController(@ModelAttribute("emp") Employee employee) {
 		ModelAndView modelAndView=new ModelAndView();
-		
-		Employee employee=new Employee();
-		employee.setEmpId(Integer.parseInt(request.getParameter("empId")));
-		employee.setEmpName(request.getParameter("empName"));
-		employee.setEmpDesignation(request.getParameter("empDesignation"));
-		employee.setEmpDepartment(request.getParameter("empDepartment"));
-		employee.setEmpSalary(Integer.parseInt(request.getParameter("empSalary")));
 		
 		String message=null;
 		if(employeeService.saveEmployee(employee))
